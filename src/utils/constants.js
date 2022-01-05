3bb613c596f66bbb6d578d6df5e058356dc01fe7
Maximilian Gaedig <mg@maximiliangaedig.com>
2022-01-05 14:08:04 +0100

    🔧 Compile and change contract address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const ERC20_DECIMALS = 18
-const chatContractAddress = "0x02ef2b5CaC2e023d0771900C34942af4d0c55798"
+const chatContractAddress = "0xF0D52c3478E438552C54F7f2a5D4ebF49D0470F4"
 const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
 
 export {ERC20_DECIMALS, chatContractAddress, cUSDContractAddress}
